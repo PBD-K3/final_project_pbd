1e69ac914a2766c499f614c4554203ba7ec4c3c5
KeiraDiaz <nkeiradiaz@gmail.com>
2024-12-16 16:03:56 +0700

    setting up api_service.dart

---
+++ thousand_flavours/lib/api_service/api_service.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'http_service.dart';
+
+class ApiService {
+  final HttpService _httpService = HttpService();
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  Future<Map<String, String>> _getHeaders() async {
+    final token = await _storage.read(key: 'auth_token');
+    return {
+      if (token != null) 'Authorization': 'Bearer $token',
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    };
+  }
+
+  Future<dynamic> get({
+    required String url,
+    Map<String, String>? queryParams,
+  }) async {
+    final headers = await _getHeaders();
+    return _httpService.get(url: url, headers: headers, queryParams: queryParams);
+  }
+}
