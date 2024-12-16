63d2471eb0656dc996931ce75a985ed49dbd8f81
KeiraDiaz <nkeiradiaz@gmail.com>
2024-12-16 16:18:04 +0700

    setting restaurants api

---
+++ thousand_flavours/lib/api_service/restaurant_api.dart
@@ -0,0 +1,12 @@
+import 'api_service.dart';
+import 'models/restaurants.dart';
+
+class RestaurantsApi {
+  final ApiService _apiService = ApiService();
+  final String _baseUrl = 'http://localhost:8000/json/';
+
+  Future<List<Restaurants>> fetchRestaurants() async {
+    final response = await _apiService.get(url: _baseUrl);
+    return (response as List).map((json) => Restaurants.fromJson(json)).toList();
+  }
+}
